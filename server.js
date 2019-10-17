33c151335fb3bebdffaaf0d6aee4af94498c60f0
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-10-17 15:36:26 -0400

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 let uri = ""
 
 // register middleware
